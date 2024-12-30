22480088907ca3afdb2bd5c3891c9f0f47aa671c
poojitha javvaji <123744284+poojitha2004@users.noreply.github.com>
2024-12-30 21:27:32 +0530

    Create about.jsx

---
+++ about.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => (
+  <section id="about">
+    <h2>About Me</h2>
+    <p>
+      Hi, I'm [Your Name]! I'm a passionate developer who loves building beautiful and functional web applications.
+    </p>
+  </section>
+);
+
+export default About;
