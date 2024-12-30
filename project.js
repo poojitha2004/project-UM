c5c1c6c3d88684af735a1d3456a006f375731b9b
poojitha javvaji <123744284+poojitha2004@users.noreply.github.com>
2024-12-30 21:27:54 +0530

    Create project.js

---
+++ project.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Projects = () => (
+  <section id="projects">
+    <h2>My Projects</h2>
+    <div>
+      <div>
+        <h3>Project 1</h3>
+        <p>Description of project 1.</p>
+      </div>
+      <div>
+        <h3>Project 2</h3>
+        <p>Description of project 2.</p>
+      </div>
+    </div>
+  </section>
+);
+
+export default Projects;
